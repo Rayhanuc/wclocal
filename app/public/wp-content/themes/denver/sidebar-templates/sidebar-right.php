41e0dcdb82051744793506f1506bb134331809b2
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-07-06 19:22:59 +0600

    ৩১.৬ - আন্ডারস্ট্র‍্যাপ বয়লারপ্লেট থিমকে রিফ্যাক্টর করে নিজের থিমের নাম অনুযায়ী নামকরন করা
    ৩১.৬ - আন্ডারস্ট্র‍্যাপ বয়লারপ্লেট থিমকে রিফ্যাক্টর করে নিজের থিমের নাম অনুযায়ী নামকরন করা

---
+++ app/public/wp-content/themes/denver/sidebar-templates/sidebar-right.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The right sidebar containing the main widget area.
+ *
+ * @package denver
+ */
+if (is_shop()) {
+	$denver_sidebar = 'shop-right-sidebar';
+}else {
+	$denver_sidebar = 'right-sidebar';
+}
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+if ( ! is_active_sidebar( $denver_sidebar ) ) {
+	return;
+}
+
+// when both sidebars turned on reduce col size to 3 from 4.
+$sidebar_pos = get_theme_mod( 'denver_sidebar_position' );
+?>
+
+<?php if ( 'both' === $sidebar_pos ) : ?>
+	<div class="col-md-3 widget-area" id="right-sidebar" role="complementary">
+<?php else : ?>
+	<div class="col-md-4 widget-area" id="right-sidebar" role="complementary">
+<?php endif; ?>
+<?php dynamic_sidebar( $denver_sidebar ); ?>
+
+</div><!-- #right-sidebar -->
